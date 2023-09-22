8f495c43014ddcb3ad903914193928f712bf942e
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-22 11:48:47 -0300

    update: added cors and routes  middlwares

--- src/config/server.js
+++ src/config/server.js
@@ -5,10 +5,13 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express()
+const cors = require('./cors');
+const routes = require('./routes')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
+server.use(cors)
+app.use('/', routes);
 
 app.listen(port, function(){
     console.log(`⚡️[server]: iniciado na porta: ${port}`)
