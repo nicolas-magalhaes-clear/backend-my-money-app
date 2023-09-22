605453ac201777023f56e3d59ec78a1dd2a58606
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-22 11:48:05 -0300

    added: schemas defined

--- src/api/billingCycle.js
+++ src/api/billingCycle.js
@@ -8,14 +8,19 @@ const creditSchema = new Schema({
 const debitSchema = new Schema({
     name: {type: String, required: true }, 
     value: {type: Number, min: 0, required: true},
-    status: {type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
-})
+    status: {type: String, required: false, uppercase: true, enum: ["PAGO", 'PENDENTE', 'AGENDADO'] }
+});
 
 const billingCycleSchema = new Schema({
     name: {type: String, required: true },
-    mont: {type: Number, min:1, max:12, required: true}
-})
+    month: {type: Number, min:1, max:12, required: true},
+    year: {type: Number, min: 1970, max:2100, required: true},
+    credits: [creditSchema],
+    debts: [debitSchema]
+});
 
-const creditModel = mongoose.model('creditModel', creditSchema)
-const debitModel = mongoose.model('debitModel', debitSchema)
-const billingCycleModel = mongoose.model('billingCycleModel', billingCycleSchema)
+const creditModel = mongoose.model('creditModel', creditSchema);
+const debitModel = mongoose.model('debitModel', debitSchema);
+const billingCycleModel = mongoose.model('billingCycleModel', billingCycleSchema);
+
+module.exports = billingCycleModel;
