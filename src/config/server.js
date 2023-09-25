d75a3518ba720c4824effedee4b5221060c27f9e
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-25 08:04:39 -0300

    update: queryParserInt added

--- src/config/server.js
+++ src/config/server.js
@@ -7,9 +7,11 @@ const express = require('express');
 const app = express()
 const cors = require('./cors');
 const routes = require('./routes')
+const queryParser = require('express-query-int')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(queryParser())
 server.use(cors)
 app.use('/', routes);
 
